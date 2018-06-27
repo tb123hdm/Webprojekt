2405432238c5005223367c137054c6d07bc7dbef
Sibi <sebastian.groener@me.com>
2018-06-27 13:12:58 +0200

    Footer jetzt an Hauptseite angepasst, und Referenz auf Registrieren.php muss aber vermutlich nochmal überarbeitet werden da nicht in allen Browsern richtig dargestellt.

--- login.php
+++ login.php
@@ -12,11 +12,6 @@ catch (PDOException $p)
 }
 
 
-$userid = $_SESSION['user'];
-if(!isset($userid)){
-    header('Location: cover.html');
-}
-
 if(isset($_POST['absenden'])){
     $email = strtolower($_POST['email']);
     $passwort = $_POST['passwort'];
@@ -70,10 +65,11 @@ if(isset($_POST['absenden'])){
 
 </head>
 
+
 <body class="text-center">
 
 
-	
+
 <section id="headline">
   <h1> Hey willkommen bei Cleo</h1>
 </section>
@@ -91,7 +87,7 @@ if(isset($errorMessage)) {
   <h2 class="h3 m-0 font-weight-light"><strong>Bitte anmelden</strong></h2>
   <label for="inputEmail" class="sr-only">Email</label>
   <input type="email" name="email" class="form-control" placeholder="Deine Email Adresse" required autofocus>
-	  
+
   <label for="inputPassword" class="sr-only">Passwort</label>
   <input type="password" name="passwort" class="form-control" placeholder="Passwort" required>
 
@@ -111,17 +107,17 @@ if(isset($errorMessage)) {
 
 
 <!-- Footer -->
-<section id="footer">
+<footer class="footer">
     <div class="container">
         <div class="row text-center text-xs-center text-sm-left text-md-left">
-            <div class="col-xs-12 col-sm-4 col-md-4">
+            <div class="col-xs-12 col-sm-4 col-md-4 mt-8">
                 <h3>C L E O</h3>
                 <br>
                 <p>Hochschule der Medien<br>
                     Nobelstraße 10<br>
                     70569 Stuttgart</p>
             </div>
-            <div class="col-xs-12 col-sm-4 col-md-4">
+            <div class="col-xs-12 col-sm-4 col-md-4 mt-8">
                 <h5>Allgemeines</h5>
                 <ul class="list-unstyled quick-links">
                     <li><a href="über_uns.html"><i class="fa fa-angle-double-right"></i>Über uns</a></li>
@@ -137,13 +133,15 @@ if(isset($errorMessage)) {
             </div>
             </br>
         </div>
-    </div>
-</section>
+
+
+</footer>
 <!-- ./Footer -->
 
-<!-- Optional JavaScript --> 
-<!-- jQuery first, then Popper.js, then Bootstrap JS --> 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> 
+</div>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
 <script src="js/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>

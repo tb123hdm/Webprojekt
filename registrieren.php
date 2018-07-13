a94ce1c17a23e6e8c6e1106bd5dbe715f035546c
Sibi <sebastian.groener@me.com>
2018-07-13 16:51:43 +0200

    Dateien können jetzt angezeigt werden

--- registrieren.php
+++ registrieren.php
@@ -63,7 +63,7 @@ if(isset($_POST['absenden'])):
 
   $passworterror = '
       <div class="alert alert-secondary alert-dismissible fade show" role="alert">
-        <h3 class="alert-heading"><i class="fa-sad-tear"></i></h3>
+        <h3 class="alert-heading"><i class="fas fa-sad-tear"></i></h3>
         <hr>
         <h4> Oh crap! Deine Daten sind leider nicht gültig.</h4>
         <p class="mb-0"> <strong> Deine Passwörter </strong> stimmen nicht miteinander überein.</p>
